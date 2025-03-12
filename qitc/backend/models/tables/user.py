0cb0db007d894851815835d8d0aa607bd56f6f98
E1 <118753603+ivanboitsov@users.noreply.github.com>
2025-03-12 04:01:14 +0300

    Модель пользователя для бд

---
+++ qitc/backend/models/tables/user.py
@@ -0,0 +1,28 @@
+import uuid
+
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import Column, Integer, String, DateTime, func, Enum
+from db.db_init import Base
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String(254), nullable=False, unique=True, index=True)
+    password = Column(String, nullable=False)
+    role = Column(Enum('user', 'student', 'admin', name='user_role'), nullable=False, default='user')
+    user_date_auth = Column(DateTime, server_default=func.now())
+
+    def __repr__(self):
+        return f"<User(id={self.id}, name='{self.name}', email='{self.email}', password='{self.password}', role='{self.role}', user_date_auth='{self.user_date_auth}')>"
+
+# Отозванные сертификаты
+class CRL(Base):
+    __tablename__ = "crl"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True, nullable=False)
+    token = Column(String, index=True, nullable=False)
+
+    def __repr__(self):
+        return f"<crl(id={self.id}, token='{self.token}')>"
