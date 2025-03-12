fcfc61c7ef8772d32cdaf40457b7177cd2a5f28b
E1 <118753603+ivanboitsov@users.noreply.github.com>
2025-03-12 04:04:18 +0300

    Дополнительные настройки конфигурации

--- qitc/backend/server.py
+++ qitc/backend/server.py
@@ -8,9 +8,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from db.db_init import db_init
 
-from routers.task_router import task_router
-from routers.course_router import course_router
 from routers.applicatoin_router import application_router
+from routers.course_router import course_router
+from routers.task_router import task_router
+from routers.user_router import user_router
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -20,10 +21,11 @@ router = APIRouter(prefix="/api/v1/qitc")
 router.include_router(application_router)
 router.include_router(course_router)
 router.include_router(task_router)
+router.include_router(user_router)
 
 app = FastAPI()
 
-# Настройка CORS
+# Настройка CORS для локального фронта
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
